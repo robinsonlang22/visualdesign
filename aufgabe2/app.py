1b94abc97b838d5157cae24918c0476e3b8b2381
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2025-05-16 15:56:06 +0200

    1605

---
+++ aufgabe2/app.py
@@ -0,0 +1,27 @@
+from IPython import get_ipython
+from IPython.display import display
+import pandas as pd
+import plotly.express as px
+
+from dash import Dash, dcc, Input, Output, html
+
+df = pd.read_csv('wein.csv', sep=',', encoding='utf-8')
+print(df.dtypes)
+columns_need_clean = ['Alkohol','Apfelsaeure','Asche','Aschen_Alkanitaet','Alle_Phenole',
+                      'Flavanoide','Proanthocyanide','Farbintensitaet','Farbwert','Proteinwert']
+
+# df['Alkohol'] = df['Alkohol'].astype(str).str.rstrip('.')
+# df['Alkohol'] = pd.to_numeric(df['Alkohol'], errors='coerce')
+def date_to_number(column):
+    column = column.astype(str).str.rstrip('.')
+    return pd.to_numeric(column, errors='coerce')
+
+for column in columns_need_clean:
+    df[column] = date_to_number(df[column])
+
+print(df.head(5))
+print(df.dtypes)
+
+df.reset_index(drop=True)
+df.to_csv('wein_cleaned.csv', index=False)
+
