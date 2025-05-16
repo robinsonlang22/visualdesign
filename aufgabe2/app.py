f56e38145bdd37ee9e9460a05a36db5054d7b2d5
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2025-05-16 16:01:26 +0200

    v1

--- aufgabe2/app.py
+++ aufgabe2/app.py
@@ -14,7 +14,7 @@ columns_need_clean = ['Alkohol','Apfelsaeure','Asche','Aschen_Alkanitaet','Alle_
 # df['Alkohol'] = pd.to_numeric(df['Alkohol'], errors='coerce')
 def date_to_number(column):
     column = column.astype(str).str.rstrip('.')
-    return pd.to_numeric(column, errors='coerce')
+    return pd.to_numeric(column, errors='coerce').round(2)
 
 for column in columns_need_clean:
     df[column] = date_to_number(df[column])
